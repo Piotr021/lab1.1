9247d5d391a80db960fd077e481a37ea85d5f737
Piotr <wwx21632@student.warszawa.merito.pl>
2026-05-28 23:38:24 +0200

    Green: implementacja dodatkowego rabatu dla VIP powyzej 5000 zl

--- lab1.1/DiscountCalculator.cs
+++ lab1.1/DiscountCalculator.cs
@@ -14,9 +14,11 @@ namespace lab1_1_net10
             if (order.Customer.IsVip)
                 discountRate += 0.10m;
 
-            // Zamówienie powyżej 1000 zł dostaje dodatkowe 5%.
             if (order.TotalAmount > 1000m)
-                //
+                discountRate += 0.05m;
+
+            // VIP z zamówieniem powyżej 5000 zł dostaje jeszcze dodatkowe 5%.
+            if (order.Customer.IsVip && order.TotalAmount > 5000m)
                 discountRate += 0.05m;
 
             return order.TotalAmount * discountRate;
