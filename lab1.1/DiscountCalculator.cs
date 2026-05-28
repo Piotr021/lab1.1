5ccee0ac5892de459c015481c39ab85678d12520
Piotr <wwx21632@student.warszawa.merito.pl>
2026-05-28 23:23:27 +0200

    Green: minimalna implementacja rabatu 0%

---
+++ lab1.1/DiscountCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab1_1_net10
+{
+    public class DiscountCalculator
+    {
+
+        public decimal CalculateDiscount(Order order)
+        {
+            // zwykły klient przy małym zamówieniu dostaje 0 zł rabatu.
+            return 0m;
+        }
+    }
+}
